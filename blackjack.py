5792552acb86a8f024b63551f44d5629c064faf3
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-19 20:10:01 +0100

    mas cambios

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,7 @@
 import random
 baraja = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+#funcion para repartir cartas
 def preparacion(baraja):
     primera_carta = random.choice(baraja)
     print(primera_carta)
@@ -11,6 +12,7 @@ def preparacion(baraja):
     
     return puntuacion
 
+#funcion para que juegue el crupier
 def crupier(baraja, puntuacion_crupier):
     while puntuacion_crupier < 17:
         nueva_carta_crupier = random.choice(baraja)
@@ -20,6 +22,7 @@ def crupier(baraja, puntuacion_crupier):
 
     return puntuacion_crupier
 
+#funcion para que juegue el jugador
 def player(baraja, puntuacion_jugador):
     print("¿Quieres una carta? ¿y o n?")
     respuesta = str(input())
@@ -33,11 +36,37 @@ def player(baraja, puntuacion_jugador):
     print("El jugador se planta")
     return puntuacion_jugador
 
-
+# cartas para eljugador
 jugador = preparacion(baraja)
 print("Esta es tu puntuación jugador : " + str(jugador))
+
+#cartas para la banca
 banca = preparacion(baraja)
 print("Esta es tu puntuación banca: " + str(banca))
 
+#Turno de jugador
 jugador = player(baraja, jugador)
+if jugador > 21:
+    print("Has perdido, te has pasado de 21 con: " + str(jugador))
+else:
+    print("Te plantas con: " + str(jugador))
+
+
+
+#Turno de la banca
 banca = crupier(baraja, banca)
+if banca > 21:
+    print("He perdido, me he pasado de 21 con: " + str(banca))
+else:
+    print("Me planto con: " + str(banca))
+
+#decision de quien gana
+if banca > 21 and jugador > 21:
+    print("Como nos hemos pasado, se queda en tablas.")
+else:
+    if banca > jugador:
+        print("He ganado")
+    elif banca == jugador:
+        print("Hemos quedado empate")
+    else:
+        print("Me has superado. Has ganado, felicidades.")
